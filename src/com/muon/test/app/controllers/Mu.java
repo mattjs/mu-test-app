b5666839a432ddf90808518591a72d709498dd82
Matthew Spencer <mattjs@Phasic.local>
2016-03-26 14:44:45 -0400

    test

---
+++ src/com/muon/test/app/controllers/Mu.java
@@ -0,0 +1,9 @@
+package controllers;
+
+import com.mu.Controller;
+
+public class Mu extends Controller {
+	public static void home() {
+		System.out.println("HOME");
+	}
+}
---
+++ src/com/muon/test/app/models/Test.java
@@ -0,0 +1,7 @@
+package models;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("HELLO WORLD");
+	}
+}
