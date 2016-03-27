ec7e0bdbc04db9f8086ecaff169d4e267c392d0d
Matthew Spencer <mattjs@Phasic.local>
2016-03-27 15:05:47 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Mu Test App
+===========
+
+This is a test application for the [Mu Http Server](https://github.com/mattjs/mu)
+
+Building this project requires [Bazel](http://bazel.io/)
---
+++ muon-test-app/app/models/Test.java
@@ -0,0 +1,11 @@
+package models;
+
+import com.mu.MuApplication;
+
+public class Test {
+	public static void main(String[] args) {
+		//File f = new File("conf/app.conf");
+		//System.out.println(f.exists());
+		new MuApplication();
+	}
+}
